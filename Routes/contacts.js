7a7e1953427a3e9ecbebcd1655bbc314208dc13e
VP <vpp41998@gmail.com>
2023-03-03 17:00:42 +0530

    Added backend

---
+++ Routes/contacts.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const cors = require("cors");
+const fileupload = require("express-fileupload");
+const contacts = require("../Models/contacts");
+
+const app = express();
+
+app.use(cors());
+app.use(fileupload());
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+  const page = req.query.page;
+  const total = req.query.total;
+  const alldata = await contacts.find({ user: req.user._id }).sort({ name: 1 });
+  const data = await contacts
+    .find({ user: req.user._id })
+    .sort({ name: 1 })
+    .skip((page - 1) * total)
+    .limit(total);
+  res.status(200).json({
+    alldata: alldata,
+    data: data,
+  });
+});
+
+app.get("/finduser", async (req, res) => {
+  res.status(200).json(req.user);
+});
+
+app.post("/addcontact", async (req, res) => {
+  let data = JSON.parse(req.body.data);
+  await data.forEach(async (e) => {
+    if (e.name && e.phone) {
+      await contacts.create({
+        user: req.user.id,
+        name: e.name,
+        designation: e.designation || "-",
+        company: e.company || "-",
+        email: e.email || "-",
+        industry: e.industry || "-",
+        phone: e.phone,
+        country: e.country || "-",
+      });
+    }
+  });
+  return res.status(200).json({ status: "success" });
+});
+
+app.post("/deletcontact", async (req, res) => {
+  const data = JSON.parse(req.body.data);
+  await data.forEach(async (e) => {
+    await contacts.deleteOne({ _id: e });
+  });
+  return res.status(200).json({ status: "success" });
+});
+
+app.post("/deleteonecontact", async (req, res) => {
+  await contacts.deleteOne({ _id: req.body.data });
+  return res.status(200).json({ status: "success" });
+});
+module.exports = app;
